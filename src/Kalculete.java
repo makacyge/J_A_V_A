36e8bb93794bed3bfe0ac57a83072ba5517b0a9e
Владислав <makacyge88@gmail.com>
2021-10-04 00:20:23 +0300

    Калькулятор

---
+++ src/Kalculete.java
@@ -0,0 +1,41 @@
+import java.net.SocketOptions;
+import java.util.Scanner;
+public class Kalculete {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a, b, resalts;
+        String оператор;
+
+        System.out.println("Введи число");
+        a = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Оператор(+-*/):");
+        оператор = scan.nextLine();
+
+        System.out.println("Введи число");
+        b = Double.parseDouble(scan.nextLine());
+
+        if (оператор.equals(("+")))
+        {
+            resalts = a + b;
+        }
+        else if (оператор.equals(("-")))
+        {
+            resalts = a - b;
+        }
+        else if (оператор.equals(("*")))
+        {
+            resalts = a * b;
+        }
+        else if (оператор.equals(("/")))
+        {
+            resalts = a / b;
+        }
+        else
+        {
+            resalts = 0;
+        }
+        System.out.println("resalts: " + resalts);
+
+    }
+}
